ab06d6702d89798b7f39de28b7e5729475bb3ae8
navagesh <deekondanagesh@gmail.com>
2025-07-20 19:40:31 +0530

    new files

--- mongodb.sh
+++ mongodb.sh
@@ -28,7 +28,7 @@ else
   echo "you are root user"
 fi
 
- cp mongo.repo/etc/yum.repos.d/     &>> $LOGFILE
+ cp mongo.repo/etc/yum.repos.d/mongo.repo &>> $LOGFILE
 
  VALIDATE $? "copied mongoDB Repo"
 
