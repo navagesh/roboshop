d816305f5eae049f9c81e871861d89b8202e6b7f
navagesh <deekondanagesh@gmail.com>
2025-07-20 23:42:45 +0530

    new files

--- redis.sh
+++ redis.sh
@@ -28,7 +28,7 @@ else
   echo "you are root user"
 fi
 
-dnf install https://rpms.remirepo.net/enterprise/remi-release-9.5-2.el9.remi.noarch -y &>> $LOGFILE
+dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>> $LOGFILE
 VALIDATE $? " installing remi release"
 dnf module enable redis:remi-6.2 -y &>> $LOGFILE
 VALIDATE $? " enabling redis"
