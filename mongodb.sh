ad7aea2d54567ca8409f6142b570d3e8c6197e44
navagesh <deekondanagesh@gmail.com>
2025-07-20 19:22:56 +0530

    new files

--- mongodb.sh
+++ mongodb.sh
@@ -6,7 +6,7 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-TIMESTAMP=$(DATE + %F-%H-%M-%S)
+TIMESTAMP=$(date + %F-%H-%M-%S)
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
 echo "script started executing at $TIMESTAMP"   &>> $LOGFILE
