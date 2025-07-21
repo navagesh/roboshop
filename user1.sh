6ead89026a3c4c110a6a5e045a7ff0338229f573
navagesh <deekondanagesh@gmail.com>
2025-07-22 00:40:21 +0530

    new files

--- user1.sh
+++ user1.sh
@@ -85,7 +85,7 @@ systemctl start user &>> $LOGFILE
 
 VALIDATE $? "Starting user"
 
-cp /home/ec2-user/roboshop/mongo.repo /etc/yum.repos.d/mongo.repo
+cp /home/ec2-user/roboshop/mongo.repo /etc/yum.repos.d/mongodb-org-8.0.repo
 
 VALIDATE $? "copying mongodb repo"
 
