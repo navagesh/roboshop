cf2c6704548b1b072382847b252cd4ef1ba33ca5
navagesh <deekondanagesh@gmail.com>
2025-07-22 13:16:19 +0530

    new files

--- catalogue84.sh
+++ catalogue84.sh
@@ -77,7 +77,8 @@ cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
 dnf install mongodb-mongosh -y &>>$LOG_FILE
 VALIDATE $? "Installing MongoDB Client"
 
-STATUS=$(mongosh --host mongodb.navagesh.store
+STATUS=$(mongosh --host mongodb.navagesh.store --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
+if [ $STATUS -lt 0 ]
 then
     mongosh --host mongodb.navagesh.store </app/db/master-data.js &>>$LOG_FILE
     VALIDATE $? "Loading data into MongoDB"
