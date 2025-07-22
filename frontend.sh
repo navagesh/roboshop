427aec0677971bd7d6cfdbdf9a50d40697b632d0
navagesh <deekondanagesh@gmail.com>
2025-07-22 15:20:18 +0530

    new files

--- frontend.sh
+++ frontend.sh
@@ -60,7 +60,7 @@ VALIDATE $? "unzipping frontend"
 rm -rf /etc/nginx/nginx.conf &>>$LOG_FILE
 VALIDATE $? "Remove default nginx conf"
 
-cp $/home/ec2-user/roboshop/nginx.conf/ etc/nginx/nginx.conf
+cp home/ec2-user/roboshop/nginx.conf/ etc/nginx/nginx.conf
 VALIDATE $? "Copying nginx.conf"
 
 systemctl restart nginx
