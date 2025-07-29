b87c87831838eea37ae72f8c66bafc33173028ac
navagesh <deekondanagesh@gmail.com>
2025-07-29 17:43:20 +0530

    new files

--- roboshop1.sh
+++ roboshop1.sh
@@ -13,7 +13,7 @@ do
   else
       INSTANCE_TYPE="t2.micro"
   fi
-  aws ec2 run-instances --image-id ami-0b4f379183e5706b9 --instance-type $INSTANCE_TYPE --security-group-ids sg-02691376fe9620bc7 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
+  IP_ADDRESS=$(aws ec2 run-instances --image-id ami-0b4f379183e5706b9 --instance-type $INSTANCE_TYPE --security-group-ids sg-02691376fe9620bc7 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
     echo "$i: $IP_ADDRESS"
 
     #create R53 record, make sure you delete existing record
