0f34c1aab2ee6a0f9323ac7e00d0880a4478478a
navagesh <deekondanagesh@gmail.com>
2025-07-29 16:24:41 +0530

    new files

--- roboshop1.sh
+++ roboshop1.sh
@@ -12,5 +12,7 @@ do
   else
       INSTANCE_TYPE="t2.micro"
   fi
-  aws ec2 run-instances --image-id ami-0b4f379183e5706b9 --instance-type $INSTANCE_TYPE --security-group-ids sg-02691376fe9620bc7
+  aws ec2 run-instances --image-id ami-0b4f379183e5706b9 --instance-type $INSTANCE_TYPE --security-group-ids sg-02691376fe9620bc7 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"
+  
+
 done
