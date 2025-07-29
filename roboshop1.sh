2313c91d2f9c2fe520838c065e81871d4f4fe6a3
navagesh <deekondanagesh@gmail.com>
2025-07-29 17:41:34 +0530

    new files

--- roboshop1.sh
+++ roboshop1.sh
@@ -2,17 +2,37 @@
 AMI=ami-0b4f379183e5706b9
 SG_ID=sg-02691376fe9620bc7
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
+ZONE_ID=Z024413530CGQ4R1R1UY9 # replace your zone ID
+DOMAIN_NAME="navages.store"
+
 for i in "${INSTANCES[@]}"
 do
-  echo "instance is :$i"
-
   if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
   then
       INSTANCE_TYPE="t3.small"
   else
       INSTANCE_TYPE="t2.micro"
   fi
-  aws ec2 run-instances --image-id ami-0b4f379183e5706b9 --instance-type $INSTANCE_TYPE --security-group-ids sg-02691376fe9620bc7 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"
-  
+  aws ec2 run-instances --image-id ami-0b4f379183e5706b9 --instance-type $INSTANCE_TYPE --security-group-ids sg-02691376fe9620bc7 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
+    echo "$i: $IP_ADDRESS"
 
+    #create R53 record, make sure you delete existing record
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+    {
+        "Comment": "Creating a record set for cognito endpoint"
+        ,"Changes": [{
+        "Action"              : "UPSERT"
+        ,"ResourceRecordSet"  : {
+            "Name"              : "'$i'.'$DOMAIN_NAME'"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "'$IP_ADDRESS'"
+            }]
+        }
+        }]
+    }
+        '
 done
